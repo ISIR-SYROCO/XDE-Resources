e5a58781d0d2d8aeb5773f28f2cc63cf03424347
Joseph Salini <josephsalini@gmail.com>
2013-12-11 01:48:43 +0100

    reorganize XDE-Resources to allow different installations; 1) python setup.py develop; 2) python setup.py install [--user]; 3) cmake .

---
+++ src/core.py
@@ -0,0 +1,14 @@
+
+import os
+resource_path = os.path.dirname(__file__)+os.sep+"resources"+os.sep+"urdf"+os.sep
+
+env11 = resource_path+"env11.xml"
+env12 = resource_path+"env12.xml"
+env1 = resource_path+"env1.xml"
+ground = resource_path+"ground.xml"
+kuka = resource_path+"kuka.xml"
+kuka_simple = resource_path+"kuka_simple.xml"
+sphere = resource_path+"sphere.xml"
+rx90 = resource_path+"rx90.xml"
+icub = resource_path+"icub.xml"
+icub_simple = resource_path+"icub_simple.xml"
